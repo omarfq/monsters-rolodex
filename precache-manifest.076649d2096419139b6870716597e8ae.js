e48e42a7c8f2d557473b84903a12e60de95b5f41
Omar Flores <omarenriquefq@gmail.com>
2019-08-26 14:48:58 -0400

    Updates

---
+++ precache-manifest.076649d2096419139b6870716597e8ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028a0ba6cb3f0dbdee7f59256d3018c3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b4b666b543520fa8aa8d",
+    "url": "/monsters-rolodex/static/css/main.cb58b8b1.chunk.css"
+  },
+  {
+    "revision": "01d8317fe3a3835ac7c6",
+    "url": "/monsters-rolodex/static/js/2.f4bca13b.chunk.js"
+  },
+  {
+    "revision": "b4b666b543520fa8aa8d",
+    "url": "/monsters-rolodex/static/js/main.2adb525d.chunk.js"
+  },
+  {
+    "revision": "9ea9c29140c1f80c8044",
+    "url": "/monsters-rolodex/static/js/runtime~main.3810dc4c.js"
+  }
+]);
